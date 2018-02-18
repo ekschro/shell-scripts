f6e15ea134f2179d26bdef21c6abc0729b78455c
ekschro <ekschro@protonmail.com>
2018-02-18 13:37:16 -0500

    Initial commit.

---
+++ hf.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#C language Header Format
+
+echo "//"
+echo "//  $1"                                               #file name
+echo "//  $2"                                               #project name
+echo "//"
+echo "//  Created by $3 on `date "+%m/%d/%Y"`."             #author
+echo "//  Copyright © `date "+%Y"` $4. All rights reserved."#Company Copyright
+echo "//"
